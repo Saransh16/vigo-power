22a03a776af31db7bdef1ded895936cc50689066
= <ksaransh682@gmail.com>
2020-05-19 02:24:23 +0530

    Send query email to gmail

--- public/main.js
+++ public/main.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
   $("#navbar").load("navBar.html");
 });
 
+$(document).ready(function(){
+  $("#footer").load("footer.html");
+});
+
 function moveToPage(page)
 {
   const domain = 'http://127.0.0.1:5500/public/';
@@ -61,6 +65,29 @@ function menuOpen()
     element_3.classList.add('hidden');        
 
   }
-
-  alert(menu_open);
 }
+
+function navBarClicked()
+{
+  console.log(document.getElementById('navItems').children);
+}
+
+function sendQuery()
+{
+  var templateParams = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('about').value
+  };
+ 
+  emailjs.send('gmail', 'template_apYv6lqh', templateParams)
+    .then(function(response) {
+      document.getElementById("queryForm").reset();      
+      alert("Your query has been sent. We will contact you soon");      
+      console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+      document.getElementById("queryForm").reset();      
+      alert("Unable to send query at the moment. Please try again later.");
+  });
+}
+
